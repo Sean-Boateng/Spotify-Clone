c6953e5095b20d48da84ea7f2dace746ee6fbc6b
Sean <seanboateng2@gmail.com>
2022-12-12 16:12:21 -0500

    linked music library to react app via axios + Created navbar file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { useEffect, useState } from "react";
+import React,{ useEffect, useState } from "react";
 import axios from 'axios';
 import MusicTable from "./Components/MusicTable";
+import NavBar from './Components/NavBar'
+
 
 
 function App(){
   const[songs,setSongs]= useState([{title:"mountain", artist:"drake", album: "unk", release_date: "today", genre:"rock"}])
+  
+  useEffect(()=>{
+    getAllSongs();
+  },[])
 
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data);
+  }
   return (
     <div>
+      <NavBar/>
       <MusicTable parentSongs={songs}/>
-    </div>
-  );
+  </div>
+);
 }
 
 
 export default App;
+
+
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+
+
+const NavBar = (props) => {
+    return ( 
+        <div>
+            <h2>Spotify</h2>
+        </div>
+     );
+}
+ 
+export default NavBar;
+
